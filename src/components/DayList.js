872b2db405798e7df001f35773e84d7d904aeb99
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-28 19:25:14 -0400

    Removed unneccesary spacing

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,8 +2,6 @@ import React from "react";
 // import classnames from 'classnames';
 import DayListItem from "components/DayListItem";
 
-
-
 export default function DayList(props) {
   const days = props.days.map(day => {
   return (
