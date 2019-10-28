bbdc89e72328a16813e6e730b2eab2d15cd70702
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-28 16:27:34 -0400

    Listing Days I - Interview Scheduler Walkthrough

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,21 @@
-// import React from "react";
-// import classnames from 'classnames';
+import React from "react";
+import classnames from 'classnames';
 
-// import "DayListItem.scss";
+import "components/DayListItem.scss";
 
-// export default function DayListItem(props) {
-//   return (
-//     <li onClick={() => props.setDay(props.name)}>
-//       <h2 className="text--regular">{props.name}</h2>
-//       <h3 className="text--light">{props.spots}</h3>
-//     </li>
-//   );
-// }
+// Assisted by Victoria Johns
+export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  return (
+    <li 
+    className={dayClass}
+    onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots}</h3>
+    </li>
+  );
+}
