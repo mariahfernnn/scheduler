58a343b435cd0f133112ec8e8e35287550a230a1
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-29 12:40:48 -0400

    Imported the Status component into stories/index.js

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -155,6 +156,9 @@ storiesOf("InterviewerListItem", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
         />
+    ))
+    .add("Status", () => (
+    <Status />
     ))
 
   
