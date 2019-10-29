5bd04e04b71ed7fe80b48cfb29af6e41c3d1b682
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-29 14:57:50 -0400

    Added a reset() function to the Form component that calls setName() and setInterviewer(null)

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,15 +6,22 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("")
+    setInterviewer(null)
+  }
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
     <form autoComplete="off">
       <input
         className="appointment__create-input text--semi-bold"
-        name={setName}
+        name="name"
+        value={name}
         type="text"
         placeholder="Enter Student Name"
+        onChange={setName}
         /*
           This must be a controlled component
         */
@@ -24,8 +31,8 @@ export default function Form(props) {
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
-      <Button danger>Cancel</Button>
-      <Button confirm>Save</Button>
+      <Button onClick={props.onCancel} danger>Cancel</Button>
+      <Button onClick={props.onSave} confirm>Save</Button>
     </section>
   </section>
 </main>
