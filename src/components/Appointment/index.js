72267f816e7e403f3d4d83be97cdc8b640850645
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-29 15:40:54 -0400

    Imported the Header, Show and Empty components into Appointment/index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 import "components/Appointment/styles.scss";
+
 import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
   return (
