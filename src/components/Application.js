b3366e52e2b08099c61358dfbb7dde16600bafa3
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-29 16:55:19 -0400

    Added appointments to src/components/Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,53 @@ import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Elizabeth James",
+      interviewer: {
+        id: 1,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Hallie Parker",
+      interviewer: {
+        id: 1,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
+];
+
 const days = [
   {
     id: 1,
