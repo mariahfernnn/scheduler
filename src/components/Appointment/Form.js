f5bf8cd3ab4628d2c74f2fa633d6d65990c7a7db
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-29 14:15:39 -0400

    Updated the Form component to add the name and interviewer to state using setName and setInterviewer

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,13 +4,15 @@ import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
     <form autoComplete="off">
       <input
         className="appointment__create-input text--semi-bold"
-        name="name"
+        name={setName}
         type="text"
         placeholder="Enter Student Name"
         /*
@@ -18,7 +20,7 @@ export default function Form(props) {
         */
       />
     </form>
-    <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={props.setInterviewer} />
+    <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={setInterviewer} />
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
