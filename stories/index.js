cf3e4d5e063108b45eaa22b8313e0988128b66e1
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-29 09:26:26 -0400

    Imported the Show component into stories/index.js

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -139,6 +140,7 @@ storiesOf("InterviewerListItem", module)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
     .add("Header", () => <Header time="12pm" />)
     .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+    .add("Show", () => <Show />)
   
 
 
