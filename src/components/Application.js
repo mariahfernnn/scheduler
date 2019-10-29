bd901f5c065d20c891fe323ad648e237c526c03e
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-29 17:47:49 -0400

    Imported components/Appointment; mapped over the appointments array to create a list in the schedule section in the schedule <section> of the Application component; Added last  Appointment to the end of the list representing the last appointment for the day

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+
 import "components/Application.scss";
+
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 const appointments = [
   {
@@ -69,31 +72,40 @@ const days = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-  return (
-    <main className="layout">
-      <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList
-  days={days}
-  day={day}
-  setDay={setDay}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
-      </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
-    </main>
-  );
+  const apps = appointments.map(apps => {
+    return (
+      <Appointment
+      key={apps.id}
+      {...apps}
+      />
+    )
+  })
+    return (
+      <main className="layout">
+        <section className="sidebar">
+        <img
+    className="sidebar--centered"
+    src="images/logo.png"
+    alt="Interview Scheduler"
+  />
+  <hr className="sidebar__separator sidebar--centered" />
+  <nav className="sidebar__menu">
+  <DayList
+    days={days}
+    day={day}
+    setDay={setDay}
+  />
+  </nav>
+  <img
+    className="sidebar__lhl sidebar--centered"
+    src="images/lhl.png"
+    alt="Lighthouse Labs"
+  />
+        </section>
+        <section className="schedule">
+          {apps}
+          <Appointment key="last" time="5pm" />
+        </section>
+      </main>
+  )
 }
