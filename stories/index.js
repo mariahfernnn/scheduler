22e3b4d9924706e608b1ce95fab72590d6af39fe
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-29 12:20:12 -0400

    Imported the Confirm component into stories/index.js

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
