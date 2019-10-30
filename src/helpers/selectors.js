e6fc6b7ebb6fe4b11b1e824ca7a1e8bef75ac8f5
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-30 18:41:36 -0400

    Moved helpers into src directory

---
+++ src/helpers/selectors.js
@@ -0,0 +1,13 @@
+export default function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.filter(days => days.name === day);
+  const dayAppts = filteredDays[0];
+
+  if (!dayAppts || dayAppts.length === 0) {
+    return [];
+  } else {
+    const appt = dayAppts.appointments.map(id => 
+      state.appointments[id]
+    )
+    return appt;
+  }
+}
