3c932bafc8415c7f1e1e942b23501c69ba9156e4
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-30 15:31:23 -0400

    Transforming Data with Selectors - Interview Scheduler Assignment : created the getAppointmentsForDay function and moved the selectors.test.js into the tests directory

--- helpers/selectors.js
+++ helpers/selectors.js
@@ -1,3 +1,13 @@
 export function getAppointmentsForDay(state, day) {
-  
+  const filteredDays = state.days.filter(days => days.name === day);
+  const dayAppts = filteredDays[0];
+
+  if (!dayAppts || dayAppts.length === 0) {
+    return [];
+  } else {
+    const appt = dayAppts.appointments.map(id => 
+      state.appointments[id]
+    )
+    return appt;
+  }
 }
