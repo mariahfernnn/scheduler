b52da4a20e605b5dc1b69bc9b2cc7dece14d45be
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-30 18:00:03 -0400

    Added key={day.id} on line 9

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import classnames from 'classnames';
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
   const days = props.days.map(day => {
   return (
-  <ul key={day.name}>
+  <ul>
     <DayListItem 
+      key={day.id}
       name={day.name} 
       spots={day.spots} 
       selected={day.name === props.day}
