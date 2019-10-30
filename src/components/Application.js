6c934f6a606df8ea2e3493abfdf52b938a766c3c
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-29 23:49:16 -0400

    Removed commented out days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -53,26 +53,16 @@ const appointments = [
   }
 ];
 
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
 export default function Application(props) {
   const [day, setDay] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days')
+    .then(response => {
+      setDay(response.data.day);
+    })
+  }, []);
+  
   const apps = appointments.map(apps => {
     return (
       <Appointment
