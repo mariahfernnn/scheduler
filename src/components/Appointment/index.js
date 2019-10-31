3d42f7a324c041b9632acf25601cebb067dd73bf
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-31 15:29:39 -0400

    Using our Custom Hook - Interview Scheduler Assignment : imported useVisualMode into the Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,13 +6,30 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 
+import useVisualMode from "hooks/useVisualMode";
+
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
   return (
     <article className="appointment">
       <Header time={props.time}/>
       <div>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onEdit={props.onEdit}
+          onDelete={props.onDelete}
+        />
+      )}
       </div>
     </article>
   );
