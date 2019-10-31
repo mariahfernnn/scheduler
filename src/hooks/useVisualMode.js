d7f28a2b7ed763cc1f8a84baf7f3170aa463fcf7
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-31 14:04:08 -0400

    Added the third constant and tests to the test file and added a back() function to the Hook that allows us to go back to the previous mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,12 +1,30 @@
 import React, { useState } from "react";
-import Show from "components/Appointment/Show";
 
 export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
-    return { 
-      mode, 
-      transition: (mode) => setMode(mode)
+  const [history, setHistory] = useState([initialMode]);
+
+  function transition(mode) {
+    setHistory(history => [mode, ...history])
+    setMode(mode)
+  }
+
+  function back() {
+    setHistory(prev => {
+      (history.splice(0, 1));
+      setMode(prev[0]);
+      return[...prev];
+    })
+  }
+  
+  return { 
+    mode, 
+    transition,
+    back
   }
 }
 
+
 // Transition function - assisted by Alex Wilmer(instructor)
+// Avoid using push/pop in React!!! - use the spread 
+// Back function - assisted by Vasily Klimkin(mentor)
