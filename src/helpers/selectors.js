ca3c29ff1c2a2dfe6d677ef51ce4b72d987f7444
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-10-30 20:43:36 -0400

    Implemented the getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
   const filteredDays = state.days.filter(days => days.name === day);
   const dayAppts = filteredDays[0];
 
@@ -10,4 +10,24 @@ export default function getAppointmentsForDay(state, day) {
     )
     return appt;
   }
+}
+
+/*
+- Create a function getInterview(state, interview)
+- Should return a new obj containing the interview data when we pass it an obj that contains the interviewer
+- Otherwise - return null
+*/
+
+// Assisted by Guy Tonye(mentor)
+export function getInterview(state, interview) {
+  let x = Object.keys(state.appointments)
+  
+  for (let y of x) {
+    if (state.appointments[y].interview !== null && interview !== null && interview.student === state.appointments[y].interview.student) {
+      return {
+        student : interview.student,
+        interviewer : state.interviewers[String(interview.interviewer)]
+      }
+    }
+  } 
 }
