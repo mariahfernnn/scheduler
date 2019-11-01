ff72c05700fae0078bb20b500a42d8d611c62d0e
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-01 17:41:17 -0400

    Using Reducers with React - Interview Scheduler Assignment : Removed useState from useApplicationData and replaced it with useReducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -5,14 +5,43 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 
 export default function useApplicationData(props) {
-  const [state, setState] = useState({
+  const initialState = {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
-  });
+  };
+
+  // For
+  // const SET_DAY = "SET_DAY";
+  // const BOOK_INTERVIEW = "BOOK_INTERVIEW";
+  // const CANCEL_INTERVIEW = "CANCEL_INTERVIEW";
+  // const SCHED_API = "SCHED_API";
+
+  function reducer(state, action) {
+    if (action.type === "SET_DAY") {
+      return { ...state, day : action.payload };
+    }
+    if (action.type === "BOOK_INTERVIEW") {
+      return {...state, appointments : action.payload };
+    }
+    if (action.type === "CANCEL_INTERVIEW") {
+      return {...state, appointments : action.payload };
+    }
+    if (action.type === "SCHED_API") {
+      return {...state, ...action.payload}
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [state, setState] = useState({
+  //   day: "Monday",
+  //   days: [],
+  //   appointments: {},
+  //   interviewers: {}
+  // });
   
-  const setDay = day => setState({ ...state, day });
+  // const setDay = day => setState({ ...state, day });
     
   useEffect(() => {
     // Use Promise.all to make both requests(for the days and the appointments data) before updating the state
