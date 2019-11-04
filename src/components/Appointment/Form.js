aad9532a92267bf9c3e11932ecca4ce925b20b92
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-04 16:12:33 -0500

    Implemented the third and fourth Form test; added the validate function provided by Compass in the body of the Form component; added the error state to the Form component; placed the JSX between the input and InterviewerList in the Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,17 +4,28 @@ import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
+  console.log("WHAT IS PROPS", props)
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  const [error, toggleError] = useState(false);
-
+  // const [error, toggleError] = useState(false);
+  const [error, setError] = useState("");
+  
   // Assisted by Vasily Klimkin(mentor)
-  const onSave = () => {
-    if (name && interviewer) {
-      props.onSave(name, interviewer)
-    } else {
-      toggleError(true)
+  //   const onSave = () => {
+  //     if (name && interviewer) {
+  //       props.onSave(name, interviewer)
+  //     } else {
+  //       toggleError(true)
+  //   }
+  // }
+
+function validate() {
+  if (name === "") {
+    setError("Student name cannot be blank");
+    return;
   }
+
+  props.onSave(name, interviewer);
 }
 
   const reset = () => {
@@ -29,9 +40,6 @@ export default function Form(props) {
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
     <form onSubmit={event => event.preventDefault()}>
-      {error && <h3><font color="red">
-        You're missing something!
-        </font></h3>}
       <input
         className="appointment__create-input text--semi-bold"
         name="name"
@@ -44,6 +52,10 @@ export default function Form(props) {
           This must be a controlled component
         */
       />
+      <section className="appointment__validation">{error}</section>
+      {/* {error && <h3><font color="red">
+        You're missing something!
+        </font></h3>} */}
     </form>
     <InterviewerList 
       interviewers={props.interviewers} 
@@ -55,7 +67,7 @@ export default function Form(props) {
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button onClick={cancel} danger>Cancel</Button>
-      <Button confirm onClick={() => onSave()}>Save</Button>
+      <Button confirm onClick={() => validate()}>Save</Button>
     </section>
   </section>
 </main>
