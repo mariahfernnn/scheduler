fe4fee67403030b089da5c4872fb11a5580eb5ea
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-04 20:43:57 -0500

    Easier Async - Interview Scheduler Reading : replaced the existing test with the updated async/await syntax

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,12 +6,23 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+// Uses the async/await syntax
+it("changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
-  
-  return waitForElement(() => getByText("Monday"))
-  .then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  })
+
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
+
+// it("defaults to Monday and changes the schedule when a new day is selected", () => {
+//   const { getByText } = render(<Application />);
+  
+//   return waitForElement(() => getByText("Monday"))
+//   .then(() => {
+//     fireEvent.click(getByText("Tuesday"));
+//     expect(getByText("Leopold Silvers")).toBeInTheDocument();
+//   })
+// });
