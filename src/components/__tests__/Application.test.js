9f0f297e553e959e8852218c0956289881e63024
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-04 21:02:34 -0500

    Integration Testing - Interview Scheduler Assignment : added a new test to Application.test.js file and added a describe block to group the tests together

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,23 +6,26 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-// Uses the async/await syntax
-it("changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
-
-  await waitForElement(() => getByText("Monday"));
-
-  fireEvent.click(getByText("Tuesday"));
-
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
-
-// it("defaults to Monday and changes the schedule when a new day is selected", () => {
-//   const { getByText } = render(<Application />);
+// Add a describe block with the name "Application"
+describe("Application", () => {
+  // Uses the async/await syntax
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
   
-//   return waitForElement(() => getByText("Monday"))
-//   .then(() => {
-//     fireEvent.click(getByText("Tuesday"));
-//     expect(getByText("Leopold Silvers")).toBeInTheDocument();
-//   })
-// });
+    await waitForElement(() => getByText("Monday"));
+  
+    fireEvent.click(getByText("Tuesday"));
+  
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+  //   const { getByText } = render(<Application />);
+    
+  //   return waitForElement(() => getByText("Monday"))
+  //   .then(() => {
+  //     fireEvent.click(getByText("Tuesday"));
+  //     expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  //   })
+  });
+})
