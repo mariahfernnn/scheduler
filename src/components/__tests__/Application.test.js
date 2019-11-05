f3bac158b513646255ce3be2dd1eb5216a6fc92a
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-05 12:36:48 -0500

    Added the four events needed to book an appointment - provided by Compass

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM } from "@testing-library/react"; 
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react"; 
 
 import Application from "components/Application";
 
@@ -26,5 +26,22 @@ describe("Application", () => {
 
     console.log(prettyDOM(container))
     
+    const appointments = getAllByTestId(container, "appointment");
+    console.log(prettyDOM(appointments))
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+    console.log(prettyDOM(appointment))
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
   });
+
 })
