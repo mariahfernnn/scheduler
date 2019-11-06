4d0e6b4923be965b7a9114f178ad9edfcc026206
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-06 16:36:56 -0500

    Added the 'should edit the interview' test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,23 +1,36 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
-    cy.request("GET", "/api/debug/reset");
 
-    cy.visit("/");
-    cy.contains("Monday");
-    
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset")
+    cy.visit("/")
+    cy.contains("Monday")
+  })
+
+  it("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
       .click()
 
-      .get("[data-testid=student-name-input]")
-      .type("Lydia Miller-Jones")
-      .get("[alt='Sylvia Palmer']")
-      .click()
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones")
+    cy.get("[alt='Sylvia Palmer']").click()
 
-    cy.contains("Save")
-      .click()
+    cy.contains("Save").click()
     
     cy.contains(".appointment__card--show", "Lydia Miller-Jones")
-      .contains(".appointment__card--show", "Sylvia Palmer")
+    cy.contains(".appointment__card--show", "Sylvia Palmer")
+  })
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force : true })
+
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones")
+    cy.get("[alt='Tori Malcolm']").click()
+
+    cy.contains("Save").click()
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones")
+    cy.contains(".appointment__card--show", "Tori Malcolm")
   })
 })
