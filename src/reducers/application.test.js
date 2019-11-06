84929f01c934d486b13052994ee17378e823dc93
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-06 00:17:31 -0500

    Added the code solution provided by Compass

--- src/reducers/application.test.js
+++ src/reducers/application.test.js
@@ -1,1 +1,12 @@
 import reducer, { SET_DAY, SET_INTERVIEW, SCHED_API } from "reducers/application";
+/*
+Write a test called "throws an error with an unsupported type". 
+Confirm that it throws the unsupported action error when we call the reducer with an empty state object 
+and an action with null as the value of type.
+*/
+describe("Application Reducer", () => {
+  it("thows an error with an unsupported type", () => {
+    expect(() => reducer({}, { type: null })).toThrowError(
+      /tried to reduce with unsupported action type/i
+  });
+})
