aa97d682f3d89a63d9ad11e82c7a6bc7777d02d1
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-06 12:21:41 -0500

    Navigating Days - Interview Scheduler Assignment : created a new test called 'should navigate to Tuesday'; wrote a query to find the list item for the correct day and click it

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,4 +4,9 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+  it("should navigate to Tuesday", () => {
+    cy.visit("/")
+    cy.get(".sidebar")
+    cy.get(".day-list__item").contains("Tuesday").click()
+  });
 });
