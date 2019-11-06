4d65ef4e56c06c40c02f196230a8c4f395431af0
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-05 19:45:02 -0500

    The Next Tests - Interview Scheduler Assignment : cancelling an interview - created a test plan; followed the test plan to implement the test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -3,6 +3,7 @@ import React from "react";
 import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, waitForElementToBeRemoved, queryByText } from "@testing-library/react"; 
 
 import Application from "components/Application";
+import useApplicationData from "hooks/useApplicationData";
 
 afterEach(cleanup);
 
@@ -18,6 +19,7 @@ describe("Application", () => {
   
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
+  // Booking an interview
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { container } = render(<Application />);
 
@@ -45,8 +47,29 @@ describe("Application", () => {
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
       );
-      
+
       expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
+  // Cancelling an interview
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application
+    const { container } = render(<Application />);
 
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    
+    // 3. Click the "Delete" button on the appointment you want to delete.
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+      );
+      fireEvent.click(getByAltText(appointment, "Delete"));
+      // 4. Check that the confirmation message is shown.
+      expect(getByText(appointment, "Delete the appointment?")).toBeInTheDocument();
+      // 5. Click the "Confirm" button.
+      fireEvent.click(getByText(appointment, "Confirm"));
+      console.log(prettyDOM(appointment))
+    // 6. Check that the element with the text "Deleting" is displayed.
+    // 7. Waiting until the element with the "Add" button is displayed.
+    // 6. Check that the DayListItem with the text "Monday" also has the text "2 spot remaining".
+  })
 })
