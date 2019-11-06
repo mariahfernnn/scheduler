627707b566b29fc39eacf7905189c1017b881e40
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-06 14:12:03 -0500

    Unable to refactor - added cy.contains on line 17 for the refactored code to work

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,7 +6,14 @@ describe("Navigation", () => {
   });
   it("should navigate to Tuesday", () => {
     cy.visit("/")
-    cy.get(".sidebar")
-    cy.get(".day-list__item").contains("Tuesday").click()
+    //   .get(".sidebar")
+    //   .get(".day-list__item")
+    //   .contains("Tuesday")
+    //   .click()
+    // cy.contains("li", "Tuesday")
+    //   .should("have.css", "background-color", "rgb(242, 242, 242)")
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+    cy.contains("[data-testid=day]", "Tuesday").should("have.css", "background-color", "rgb(242, 242, 242)");
   });
 });
