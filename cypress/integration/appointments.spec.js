e64fa9535b7d856d14d94d041afdd1aa3647390c
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-06 15:38:03 -0500

    Added the command to the test that clicks the add button for the empty appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,5 +1,10 @@
 describe("Appointments", () => {
   it("should book an interview", () => {
     cy.visit("/");
+    cy.contains("Monday");
+    
+    cy.get("[alt=Add]")
+    .first()
+    .click()
   })
 })
