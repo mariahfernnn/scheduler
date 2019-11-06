ab8b4fe110918ab696e3d86d89da7f2ae0b7bde6
mariahfernnn <maria.h.fernandez@ryerson.ca>
2019-11-05 23:43:28 -0500

    Removed react elements that were no longer being used

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, waitForElementToBeRemoved, queryByText, queryByAltText, wait } from "@testing-library/react"; 
+import { render, cleanup, waitForElement, fireEvent, getByText, getAllByTestId, getByAltText, getByPlaceholderText, waitForElementToBeRemoved, queryByText, queryByAltText } from "@testing-library/react"; 
 
 import Application from "components/Application";
 
